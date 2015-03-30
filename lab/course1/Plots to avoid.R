94423be6a24609b998c0449db62839c33fed5b7e
chadchouGitHub <chiachingchou@gmail.com>
2015-03-30 12:06:22 -0400

    Update
    Update

--- lab/course1/Plots to avoid.R
+++ lab/course1/Plots to avoid.R
@@ -114,7 +114,7 @@ plot(r,g,lwd=2,cex=0.2,pch=16,
      main=paste0("corr=",signif(cor(r,g),3)))
 abline(0,1,col=2,lwd=2)
 f <- function(a,x,y,p=0.95) mean(x<=a & y<=a)-p
-a95 <- uniroot(f,lower=2000,upper=20000,x=r,y=g)$root
+a95 <- uniroot(f,lower=2000,upper=20000,x=r,y=g)$root # I don't understand the root? for what!!
 abline(a95,-1,lwd=2,col=1)
 text(8500,0,"95% of data below this line",col=1,cex=1.2,adj=c(0,0))
 r <- log2(r)
@@ -124,10 +124,83 @@ plot(r,g,lwd=2,cex=0.2,pch=16,
      ylab=expression(paste(log[2], " ", E[2])),
      main=paste0("corr=",signif(cor(r,g),3)))
 abline(0,1,col=2,lwd=2)
+## Note: compare two different scale in the same data. The correlation will also different when the scale change.
+### Although the correlation is reduced in the log-scale, 
+### it is very close to 1 in both cases. 
+### Does this mean these data are reproduced?
 
 
+### To examine how well the second vector reproduces the first, we need to study the differences. 
+### So we should instead plot that. 
+### In this plot we plot the difference (in the log scale) versus the average: 
+### These are referred to as Bland-Altman plots or MA plots
+
+mypar2(1,1)
+## x axis is (r+g)/2 and y axis is (r-g) ...  
+plot((r+g)/2,(r-g),lwd=2,cex=0.2,pch=16,
+     xlab=expression(paste("Ave{ ",log[2], " ", E[1],", ",log[2], " ", E[2]," }")),
+     ylab=expression(paste(log[2]," { ",E[1]," / ",E[2]," }")),
+     main=paste0("SD=",signif(sqrt(mean((r-g)^2)),3)))
+abline(h=0,col=2,lwd=2)
+## This plot we see that the typical difference in the log (base 2) scale between two replicated measures is about 1
+## (I can't see what this mean)
+## This means that when measurements should be the same we will, on average, observe 2 fold difference.
+
+## BarPlot for pair Data
+### A common task in data analysis is the comparison of two groups. 
+### When the dataset is small and data are paired, for example outcomes before and after a treatment, 
+### an unfortunate display that is used is the barplot with two colors.
+
+###Don't use barPlot
+
+set.seed(12201970)
+before <- runif(6, 5, 8)
+after <- rnorm(6, before*1.05, 2)
+li <- range(c(before, after))
+ymx <- max(abs(after-before))
+barplot(c(before, after))
+mypar2(1,2)
+barplot(before)
+barplot(after)
 
 
+## Use scatterplot
+mypar2(1,2)
+plot(before, after, xlab="Before", ylab="After",
+     ylim=li, xlim=li)
+abline(0,1, lty=2, col=1)
 
+
+plot(before, after-before, xlab="Before", ylim=c(-ymx, ymx),
+     ylab="Change (After - Before)", lwd=2)
+abline(h=0, lty=2, col=1)
+
+
+## Line plots are not a bad choice, although I find them harder to follow than the previous two. 
+## Boxplots show you the increase, but lose the paired information.
+
+z <- rep(c(0,1), rep(6,2))
+mypar2(1,2)
+plot(z, c(before, after),
+     xaxt="n", ylab="Response",
+     xlab="", xlim=c(-0.5, 1.5))
+axis(side=1, at=c(0,1), c("Before","After"))
+segments(rep(0,6), before, rep(1,6), after, col=1)     
+
+boxplot(before,after,names=c("Before","After"),ylab="Response")
+
+
+### Gratuitous 3D ...NO NO NO 3D just curve lines..
+
+library(downloader)
+filename <- "fig8dat.csv"
+url <- "https://github.com/kbroman/Talk_Graphs/raw/master/R/fig8dat.csv"
+if (!file.exists(filename)) download(url, filename)
+x <- read.table(filename, sep=",", header=TRUE)
+plot(x[,1],x[,2],xlab="log Dose",ylab="Proportion survived",ylim=c(0,1),
+     type="l",lwd=2,col=1)
+lines(x[,1],x[,3],lwd=2,col=2)
+lines(x[,1],x[,4],lwd=2,col=3)
+legend(1,0.4,c("Drug A","Drug B","Drug C"),lwd=2, col=1:3)
 
 
